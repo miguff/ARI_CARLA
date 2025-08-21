9f14e8d70a9bd015cbbf35daf458114537c565ac
Miguff <mihgeza2000@gmail.com>
2025-08-22 00:06:30 +0200

    Lidar Sensor Setup

--- V2V_Setup/Vehicles/Vehicle.py
+++ V2V_Setup/Vehicles/Vehicle.py
@@ -1,5 +1,7 @@
 from .Network import VehicleNetwork
-
+import carla
+from Utils.LidarUrils import Lidar
+import open3d as o3d
 
 #TODO: Add Noise
 #TODO: Add Packet Loss
@@ -11,13 +13,15 @@ class CustomVehicle:
     def __init__(self, world, blueprint, spawn_point):
         self.world = world
         self.vehicle = world.try_spawn_actor(blueprint, spawn_point)  # Composition: vehicle is a member variable
-        self.sensors = []
+        self.camera = []
         self.connected_vehicles = []
+
+        self.attached_sensors = {}
 
         VehicleNetwork.register(self)
 
     def apply_control(self, control):
-        self.vehicle.apply_control(control)  # Delegate call to member vehicle
+        self.vehicle.apply_control(carla.VehicleControl(throttle=control))  # Delegate call to member vehicle
 
     def get_location(self):
         return self.vehicle.get_location()
@@ -26,13 +30,23 @@ class CustomVehicle:
         camera = self.world.spawn_actor(camera_bp, transform, attach_to=self.vehicle)
         if callback:
             camera.listen(callback)
-        self.sensors.append(camera)
+        self.camera.append(camera)
         return camera
+    
+    def attach_lidar(self, lidar_bp, lidarclass: Lidar, point_list: o3d.geometry.PointCloud, lidar_transform = carla.Transform(carla.Location(x=-0.5, z=1.8)), name = "Name", ):
+        lidar = self.world.spawn_actor(lidar_bp, lidar_transform, attach_to=self.vehicle)
+        lidar.listen(lambda data: lidarclass.lidar_callback(data, point_list))
+        self.attached_sensors[name] = lidar
+
+    def set_autopilot(self, value=True):
+        self.vehicle.set_autopilot(value)
+
+    
     def get_speed(self):
         return self.vehicle.get_speed()
 
     def destroy(self):
-        for sensor in self.sensors:
+        for sensor in self.camera:
             sensor.stop()
             sensor.destroy()
         self.vehicle.destroy()
--- V2V_Setup/test.py
+++ V2V_Setup/test.py
@@ -1,18 +1,66 @@
 import carla
 from Vehicles import CustomVehicle
+from Utils import Lidar
 import time
+import open3d as o3d
 
-client = carla.Client('localhost', 2000)
+DELTA = 0.05
 
-world = client.get_world()
-blueprint_library = world.get_blueprint_library()
-vehicle_bp = blueprint_library.filter('*mini*')[0]
-spawn_points = world.get_map().get_spawn_points()
-spawn_point = spawn_points[2]
-print(spawn_point)
-EgoVehicle = CustomVehicle(world, vehicle_bp, spawn_point)
-EgoVehicle.send_signal()
+def main():
+    """Main of the Script"""
+    #SETUP The World
+    client = carla.Client('localhost', 2000)
+    world = client.get_world()
+    settings = world.get_settings()
+    settings.fixed_delta_seconds = DELTA
+    settings.synchronous_mode = True
+    world.apply_settings(settings)
 
-time.sleep(500)
+    #PointCloud Setup
+    point_list = o3d.geometry.PointCloud()
 
-EgoVehicle.destroy()
+    blueprint_library = world.get_blueprint_library()
+    vehicle_bp = blueprint_library.filter('*mini*')[0]
+    spawn_points = world.get_map().get_spawn_points()
+    spawn_point = spawn_points[2]
+    print(spawn_point)
+
+    EgoVehicle = CustomVehicle(world, vehicle_bp, spawn_point)
+    print(type(EgoVehicle))
+    lidarClass = Lidar(world, blueprint_library, DELTA)
+    lidar_bp = lidarClass.generate_lidar_bp()
+    EgoVehicle.attach_lidar(lidar_bp, lidarClass, point_list, name="OnlyLidar")
+    EgoVehicle.send_signal()
+    EgoVehicle.set_autopilot()
+
+    #Visualize
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(
+        window_name='Carla Lidar',
+        width=960,
+        height=540,
+        left=480,
+        top=270)
+    vis.get_render_option().background_color = [0.05, 0.05, 0.05]
+    vis.get_render_option().point_size = 1
+    vis.get_render_option().show_coordinate_frame = True
+
+
+    frame = 0
+    while frame < 200:
+        if frame == 2:
+            vis.add_geometry(point_list)
+        vis.update_geometry(point_list)
+
+        vis.poll_events()
+        vis.update_renderer()
+        # # This can fix Open3D jittering issues:
+        time.sleep(0.005)
+        world.tick()
+        frame += 1
+
+    EgoVehicle.destroy()
+
+
+if __name__ == "__main__":
+    main()
